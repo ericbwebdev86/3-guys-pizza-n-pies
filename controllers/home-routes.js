6976a49b074f2caca2c94c47be0d7a2c4c0c94c8
Preston Fassbender <fassbenderp0551@gmail.com>
2022-03-07 20:28:09 -0600

    user routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 
+// route to homepage view
 router.get('/', (req, res) => {
     res.render('homepage', {
         loggedIn: req.session.loggedIn
